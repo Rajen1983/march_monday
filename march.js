bf385d58b2408d53533396f28d8318f24c4e065d
kohli <moreneekoli@gmail.com>
2024-03-16 23:41:47 +0530

    refresh

--- march.js
+++ march.js
@@ -1,25 +1,30 @@
-// let a = 11;   
-// let b = a++;
-// let c = a++;
-// let d = c++;
-// let e = b++;
-
-// console.log(a)   // ans will be 11
-// console.log(b)   // ans will be 11
-// console.log(c)    // ans will be 11
-// console.log(d)
-// console.log(e)
 
 
+var a = 5;
+var b = a++;
+console.log(a)
+console.log(b)
 
+var a = 2;
+var b = a++;
+let f = a+5;
+console.log(a)
+console.log(b)
+console.log(f)
 
+var x = 5;
+console.log(x++)
+console.log(x)
 
-// let a = 20;
-// let b = ++a;
-// let c = ++b;
+var a = 5;
+var b = a++;
+var c = b++;
+var d = c++;
+console.log(a+b+c+d)
 
-// console.log(a)   // ans will be 11
-// console.log(b)   // ans will be 11
-// console.log(c)    // ans will be 11
-
+var x = 10;
+var y = x++;
+var z = y++;
+var a = z++;
+console.log(x+y+z+a)
 
