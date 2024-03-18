39c4fa7a7085232b34696d4bb8fffa984efb4d70
kohli <moreneekoli@gmail.com>
2024-03-18 22:33:01 +0530

    monday

--- march.js
+++ march.js
@@ -1,30 +1,67 @@
 
 
-var a = 5;
-var b = a++;
-console.log(a)
-console.log(b)
+// var a = 5;
+// var b = a++;
+// console.log(a)
+// console.log(b)
 
-var a = 2;
-var b = a++;
-let f = a+5;
-console.log(a)
-console.log(b)
-console.log(f)
+// var a = 2;
+// var b = a++;
+// let f = a+5;
+// console.log(a)
+// console.log(b)
+// console.log(f)
 
-var x = 5;
-console.log(x++)
-console.log(x)
+// var x = 5;
+// console.log(x++)
+// console.log(x)
 
-var a = 5;
-var b = a++;
-var c = b++;
-var d = c++;
-console.log(a+b+c+d)
+// var a = 5;
+// var b = a++;
+// var c = b++;
+// var d = c++;
+// console.log(a+b+c+d)
 
-var x = 10;
-var y = x++;
-var z = y++;
-var a = z++;
-console.log(x+y+z+a)
+// var x = 10;
+// var y = x++;
+// var z = y++;
+// var a = z++;
+// console.log(x+y+z+a)
 
+// let light="green";
+
+// if(light=="green"){
+//     console.log("go");
+// } 
+// else{
+//     console.log("stop");
+// }
+
+// let number=10;
+// if(number>=0){
+//     console.log("possitive");
+// } else{
+//     console.log("stop");
+// }
+
+let numbers = 11;
+if(numbers % 2 == 0){
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
+
+
+let price = 99;
+if (price >= 100){
+    console.log("discount20")
+}
+
+else if(price >=50){
+    console.log("discount 10")
+}
+
+else{
+    console.log("discount 0")
+}
