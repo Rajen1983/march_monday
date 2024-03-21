f9211957ab98da65fc5d78925ddf23946c09c477
kohli <moreneekoli@gmail.com>
2024-03-21 14:30:30 +0530

    21march24

--- march.js
+++ march.js
@@ -159,25 +159,25 @@
 // }
 // console.log("discount Rs", discount);
 
-let age=17;
-let price=100;
-let inshurance=1;
-if (age <9){
-    price=0;
-}
-else if(age>=9 && age<18){
-    price=(price/2+inshurance);
-}
-else if(age>=18 && age<60){
-    price=(price+inshurance);
-}
-else if(age>=60 && age<79){
-    price=(price/2+inshurance);
-}
-else{
-     price=0;
-    }
-console.log ("ticket lagega-Rs",price)
+// let age=18;
+// let price=100;
+// let inshurance=1;
+// if (age <9){
+//     price=0;
+// }
+// else if(age>=9 && age<18){
+//     price=(price/2+inshurance);
+// }
+// else if(age>=18 && age<60){
+//     price=(price+inshurance);
+// }
+// else if(age>=60 && age<80){
+//     price=(price/2+inshurance);
+// }
+// else{
+//      price=0;
+//     }
+// console.log ("ticket lagega-Rs",price)
 
 // let age = 17;
 // let price = 100;
@@ -196,3 +196,65 @@ console.log ("ticket lagega-Rs",price)
 // }
 
 // console.log("Ticket lagega - Rs", price);
+
+// let time=1800;
+// if (time <1200){
+//     message="good morning";
+// }
+// else if(time>=1200 && time<1600){
+//     message="good afternoon";
+// }
+// else if(time>=1600 && time<1900){
+//     message="good evening";
+// }
+// else if(time>=1900 && time<2130){
+//     message="good night";
+// }
+// else{
+//     message="so ja DK BOSS Raat ho gyi he";
+//     }
+// console.log ("message---",message)
+
+// let a=10;
+// let b=20;
+// a==b?console.log("equal"):console.log("not equal")
+
+// let a="data1";
+// let b="data1";
+// let c="data3";
+// a==b?console.log("true"):console.log(false)
+// a==b?console.log("true"):a==c?console.log(true):console.log(false)
+
+// let age=40;
+
+// age==40?console.log("younger"):age>40?console.log("order"):console.log("minor")
+// // a==b?console.log("true"):a==c?console.log(true):console.log(false)
+
+// let per=91;
+
+// per>=90?console.log("A"):(per>=80 && per<89) ?console.log("B"):(per>=70 && per<79) ?console.log("c"):
+// (per>=60 && per<69) ?console.log("D"):console.log("fail")
+
+// let number=-10;
+// number<0?console.log("Negative"):console.log("positive")
+
+// Function to find the greatest of three numbers using ternary operator
+    // let  a = 2;
+    // let  b = 5;
+    // let  c = 8;
+    // (a > b && a > c) ? console.log ("a") : (b > a &&  b>c) ? console.log ("b") :  console.log ("c");
+
+// let isloogged = "false";
+// (isloogged==="true")? console.log("welcome"):console.log("please log in")
+
+// let dayOfWeek = "monday";
+// let day;
+
+// if (dayOfWeek === "saturday" || dayOfWeek === "sunday") {
+//     day = "not weekend";
+// } else {
+//     day = "weekend";
+// }
+
+// console.log(day); 
+
